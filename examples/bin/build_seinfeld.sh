87b737768472d59e18a95f4c5b1475214d5846ed
Frank Scholten <frank@frankscholten.nl>
2011-02-01 00:30:57 +0100

    * Preprocessed Seinfeld scripts * Added SpeakerFilterTest * Finished analyzer * Added FuzzyKMeans * Added ClusterDistances

--- examples/bin/build_seinfeld.sh
+++ examples/bin/build_seinfeld.sh
@@ -1,29 +1,49 @@
 #!/bin/sh
 
-INPUT=src/main/resources/seinfeld-scripts
+INPUT=src/main/resources/seinfeld-scripts-preprocessed
 OUTPUT=out/seinfeld-scripts
 
-mahout seqdirectory --input     $INPUT \
-                    --output    $OUTPUT/seqfiles \
-                    --charset    utf-8
+#mahout seqdirectory --input             $INPUT \
+#                    --output            $OUTPUT/seqfiles \
+#                    --charset           utf-8
+
+#wait
+
+mahout seq2sparse   --input             $OUTPUT/seqfiles    \
+                    --output            $OUTPUT/vectors     \
+                    --maxNGramSize      2                   \
+                    --namedVector                           \
+                    --minDF             4                   \
+                    --maxDFPercent      50                  \
+                    --weight            TFIDF               \
+                    --norm              2                   \
+                    --analyzerName      org.apache.mahout.analysis.SeinfeldScriptAnalyzer
 
 wait
 
-mahout seq2sparse   --input           $OUTPUT/seqfiles    \
-                    --output          $OUTPUT/vectors     \
-                    --maxNGramSize    2                   \
-                    --namedVector                         \
-                    --minDF           4                   \
-                    --maxDFPercent   50                   \
-                    --weight          TFIDF               \
-                    --norm            2                   \
-                    --analyzerName   org.apache.mahout.analysis.SeinfeldScriptAnalyzer
-
-wait
-
-mahout kmeans       --input         $OUTPUT/vectors/tfidf-vectors \
-                    --output        $OUTPUT/clusters \
-                    --clusters      $OUTPUT/initialclusters \
-                    --maxIter       25 \
-                    --numClusters   25 \
+mahout kmeans       --input            $OUTPUT/vectors/tfidf-vectors \
+                    --output           $OUTPUT/clusters \
+                    --clusters         $OUTPUT/initialclusters \
+                    --distanceMeasure
+                    --maxIter          10 \
+                    --numClusters      100 \
                     --clustering
+                    --overwrite
+
+#mahout fkmeans      --input             $OUTPUT/vectors/tfidf-vectors \
+#                    --output            $OUTPUT/clusters \
+#                    --clusters          $OUTPUT/initialclusters \
+#                    --distanceMeasure   org.apache.mahout.common.distance.CosineDistanceMeasure \
+#                    --maxIter           10 \
+#                    --numClusters       100 \
+#                    --clustering            \
+#                    --m                 2   \
+#                    --overwrite
+
+wait
+
+#mahout clusterdump  --seqFileDir        $OUTPUT/clusters/clusters-1 \
+#                    --pointsDir         $OUTPUT/clusters/clusteredPoints \
+#                    --numWords          5 \
+#                    --dictionary        $OUTPUT/vectors/dictionary.file-0 \
+#                    --dictionaryType    sequencefile
--- examples/src/main/java/org/apache/mahout/analysis/SeinfeldScriptAnalyzer.java
+++ examples/src/main/java/org/apache/mahout/analysis/SeinfeldScriptAnalyzer.java
@@ -2,8 +2,10 @@ package org.apache.mahout.analysis;
 
 import org.apache.lucene.analysis.*;
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.analysis.standard.StandardFilter;
 import org.apache.lucene.util.Version;
 
+import java.io.IOException;
 import java.io.Reader;
 import java.lang.Override;import java.lang.String;import java.lang.SuppressWarnings;import java.util.Arrays;
 import java.util.List;
@@ -15,36 +17,37 @@ import java.util.Set;
  *
  * @author Frank Scholten
  */
-public class SeinfeldScriptAnalyzer extends StandardAnalyzer {
+public class SeinfeldScriptAnalyzer extends Analyzer {
 
   final List<String> stopWords = Arrays.asList(
           "a", "an", "and", "are", "as", "at", "be", "but", "by",
-          "do", "don't", "for", "he", "i", "if", "in", "i'm", "into", "is", "it", "know", "me",
+          "do", "don't", "for", "he", "here", "i", "if", "in", "i'm", "into", "is", "it", "know", "me",
           "no", "not", "oh", "of", "on", "or", "such",
-          "get", "just", "from", "about",
-          "that", "the", "their", "then", "there", "these",
-          "they", "this", "to", "what", "was", "we", "will", "with", "you're",
-          "jerry", "george", "elaine", "kramer", "yeah", "you", "she", "have", "her", "his", "like", "well", "out"
+          "get", "gonna", "good", "going", "just", "from", "about",
+          "that", "the", "their", "then", "there", "these", "think",
+          "they", "this", "to", "what", "was", "we", "will", "with", "you're", "your",
+          "jerry", "george", "elaine", "kramer", "want", "yeah", "you", "she", "have", "her", "his", "like", "well", "out"
   );
 
   public SeinfeldScriptAnalyzer() {
-    super(Version.LUCENE_30);
   }
 
   @SuppressWarnings("unchecked")
   @Override
   public TokenStream tokenStream(String fieldName, Reader reader) {
-    TokenStream tokenStream = super.tokenStream(fieldName, reader);
+    TokenStream tokenStream = new LetterTokenizer(reader);
+    tokenStream = new SpeakerFilter(tokenStream);
+    tokenStream = new StandardFilter(tokenStream);
+    tokenStream = new LengthFilter(tokenStream, 4, 20);
+
+    tokenStream = new LowerCaseFilter(tokenStream);
 
     final Set stopSet = new CharArraySet(stopWords.size(), true);
     stopSet.addAll(stopWords);
-
     tokenStream = new StopFilter(true, tokenStream, stopSet, true);
 
-    // TODO: Filter numbers
+    return tokenStream;
 
     // TODO: Filter mute subtitles
-
-    return new LengthFilter(tokenStream, 4, 20);
   }
 }
